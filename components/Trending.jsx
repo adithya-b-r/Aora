1ea0f26036aae676371af4ffbda900a50d6e60c2
Abr009 <bradithya945@gmail.com>
2024-12-24 13:41:33 +0530

    Fixed video not playing issue in Trending component.

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,67 +1,82 @@
-import { View, Text, FlatList, TouchableOpacity, ImageBackground, Image } from 'react-native'
-import React, { useState } from 'react'
+import { FlatList, TouchableOpacity, ImageBackground, Image, View } from 'react-native';
+import React, { useState, useCallback } from 'react';
 import * as Animatable from 'react-native-animatable';
-import {Video, ResizeMode} from 'expo-av';
-
-import { icons} from '@/constants';
+import { Video, ResizeMode } from 'expo-av';
+import { icons } from '@/constants';
 
 const zoomIn = {
   0: { scale: 0.9 },
-  1: { scale: 1.1 }
+  1: { scale: 1.1 },
 };
 
 const zoomOut = {
   0: { scale: 1.1 },
-  1: { scale: 0.9 }
+  1: { scale: 0.9 },
 };
 
-const TrendingItem = ({ activeItem, item }) => {
+const TrendingItem = ({ activeId, item }) => {
   const [play, setPlay] = useState(false);
+
+  const handleVideoError = useCallback((error) => {
+    console.error('Video Error:', error);
+    setPlay(false);
+  }, []);
 
   return (
     <Animatable.View
-      className='mr-5'
-      animation={activeItem === item.$id ? zoomIn : zoomOut}
+      className="mr-5"
+      animation={activeId === item.$id ? zoomIn : zoomOut}
       duration={500}
     >
       {play ? (
-        <Video  
-          source={{ uri: item.video }}
-          className='w-52 h-72 rounded-[35px] mt-3 bg-white/10'
-          resizeMode={ResizeMode.CONTAIN}
-          useNativeControls
-          shouldPlay
-        />
+        <View className="w-52 h-72 rounded-[35px] mt-3 overflow-hidden">
+          <Video
+            source={{ uri: "https://www.w3schools.com/html/mov_bbb.mp4" }}
+            style={{ width: '100%', height: '100%' }}
+            resizeMode={ResizeMode.COVER}
+            shouldPlay
+            isLooping
+            orientation="landscape"
+            useNativeControls
+            onError={handleVideoError}
+          />
+        </View>
       ) : (
         <TouchableOpacity
-          className='relative justify-center items-center'
+          className="relative justify-center items-center"
           activeOpacity={0.7}
-          onPress={() => setPlay(true)}>
+          onPress={() => setPlay(true)}
+        >
           <ImageBackground
             source={{ uri: item.thumbnail }}
-            className='w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40'
-            resizeMode='cover'
-          />
-
-          <Image
-            source={icons.play}
-            className='absolute w-12 h-12'
-            resizeMode='contain'
-          />
+            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
+            resizeMode="cover"
+          >
+            <View className="flex-1 justify-center items-center">
+              <Image
+                source={icons.play}
+                className="w-12 h-12"
+                resizeMode="contain"
+              />
+            </View>
+          </ImageBackground>
         </TouchableOpacity>
-      )
-      }
+      )}
     </Animatable.View>
-  )
-}
+  );
+};
 
-const Trending = ({ posts }) => {
-  const [activeItem, setActiveItem] = useState(posts[1]);
+const Trending = ({ posts = [] }) => {
+  const [activeId, setActiveId] = useState(posts[0]?.$id);
 
-  const viewableItemsChanged = ({ viewableItems }) => {
-    if (viewableItems.length > 0) {
-      setActiveItem(viewableItems[0].key);
+  const handleViewableItemsChanged = useCallback(({ viewableItems }) => {
+    if (viewableItems?.[0]?.item) {
+      setActiveId(viewableItems[0].item.$id);
     }
+  }, []);
+
+  if (!posts.length) {
+    return null;
   }
 
   return (
@@ -69,16 +84,21 @@ const Trending = ({ posts }) => {
       data={posts}
       keyExtractor={(item) => item.$id}
       renderItem={({ item }) => (
-        <TrendingItem activeItem={activeItem} item={item} />
+        <TrendingItem activeId={activeId} item={item} />
       )}
-      onViewableItemsChanged={viewableItemsChanged}
+      onViewableItemsChanged={handleViewableItemsChanged}
       viewabilityConfig={{
-        itemVisiblePercentThreshold: 70
+        itemVisiblePercentThreshold: 70,
       }}
-      contentOffset={{ x: 170 }}
-      horizontal
-    />
-  )
-}
 
-export default Trending
+      contentOffset={{ x: 170}}
+
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{ paddingHorizontal: 16 }}
+      initialScrollIndex={0}
+    />
+  );
+};
+
+export default Trending;
